cebad331b66034a09ed2b952f25c0e5cbd9a7ba2
Ashley Davis <ashley.davis@realseriousgames.com>
2015-10-23 17:31:14 +1000

    First commit of this crazy idea.

---
+++ test/test.js
@@ -0,0 +1,11 @@
+'use strict';
+
+describe('test-suite', function () {
+	
+	var expect = require('chai').expect;
+	
+	it('should be true', function () {
+		expect(true).to.equal(true);		
+	});
+	
+});
