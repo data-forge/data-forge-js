83cf255af8e6588db3857a2bbc2fa962a8e05609
Ashley Davis <ashley@codecapers.com.au>
2015-10-27 09:57:48 +1000

    Fixed variable naming.

--- test/lazydataframe.test.js
+++ test/lazydataframe.test.js
@@ -32,9 +32,8 @@ describe('LazyDataFrame', function () {
 
 	it('can get columns', function () {
 		
-		var LazyDataFrame = initExampleLazyDataFrame();
-		
-		expect(LazyDataFrame.columns()).to.eql([
+		var lazyDataFrame = initExampleLazyDataFrame();	
+		expect(lazyDataFrame.columns()).to.eql([
 			"Value1",
 			"Value2",
 			"Value3",
@@ -43,9 +42,8 @@ describe('LazyDataFrame', function () {
 	
 	it('can get index', function () {
 		
-		var LazyDataFrame = initExampleLazyDataFrame();
-		
-		expect(LazyDataFrame.index().values()).to.eql([
+		var lazyDataFrame = initExampleLazyDataFrame();		
+		expect(lazyDataFrame.index().values()).to.eql([
 			new Date(1975, 24, 2),
 			new Date(2015, 24, 2)
 		]);		
@@ -53,9 +51,8 @@ describe('LazyDataFrame', function () {
 	
 	it('can get values', function () {
 		
-		var LazyDataFrame = initExampleLazyDataFrame();
-		
-		expect(LazyDataFrame.values()).to.eql(			[
+		var lazyDataFrame = initExampleLazyDataFrame();		
+		expect(lazyDataFrame.values()).to.eql(			[
 			[100, 'foo', 11],
 			[200, 'bar', 22],
 		]);		
@@ -64,15 +61,15 @@ describe('LazyDataFrame', function () {
 	it('throws expection when pulling a non-existing column name', function () {
 		
 		expect(function () {
-			var LazyDataFrame = initExampleLazyDataFrame();
-			LazyDataFrame.series('non-existing column name');			
+			var lazyDataFrame = initExampleLazyDataFrame();
+			lazyDataFrame.series('non-existing column name');			
 		}).to.throw(Error).with.property('message').that.equals("In call to 'series' failed to find column with name 'non-existing column name'.");
 	});
 
 	it('can pull column as series', function () {
 		
-		var LazyDataFrame = initExampleLazyDataFrame();
-		var series1 = LazyDataFrame.series('Value1');
+		var lazyDataFrame = initExampleLazyDataFrame();
+		var series1 = lazyDataFrame.series('Value1');
 		expect(series1.index().values()).to.eql([
 			new Date(1975, 24, 2),
 			new Date(2015, 24, 2)
@@ -82,7 +79,7 @@ describe('LazyDataFrame', function () {
 			200,
 		]);		
 		
-		var series2 = LazyDataFrame.series('Value2');
+		var series2 = lazyDataFrame.series('Value2');
 		expect(series2.index().values()).to.eql([
 			new Date(1975, 24, 2),
 			new Date(2015, 24, 2)
@@ -92,7 +89,7 @@ describe('LazyDataFrame', function () {
 			'bar',
 		]);			
 		
-		var series3 = LazyDataFrame.series('Value3');
+		var series3 = lazyDataFrame.series('Value3');
 		expect(series3.index().values()).to.eql([
 			new Date(1975, 24, 2),
 			new Date(2015, 24, 2)
@@ -105,8 +102,8 @@ describe('LazyDataFrame', function () {
 	
 	it('can pull column subset as new LazyDataFrame', function () 
 	{
-		var LazyDataFrame = initExampleLazyDataFrame();
-		var subsetLazyDataFrame = LazyDataFrame.subset(['Value3', 'Value1']);
+		var lazyDataFrame = initExampleLazyDataFrame();
+		var subsetLazyDataFrame = lazyDataFrame.subset(['Value3', 'Value1']);
 		expect(LazyDataFrame).not.to.equal(subsetLazyDataFrame); 
 		expect(subsetLazyDataFrame.index().values()).to.eql([
 			new Date(1975, 24, 2),
@@ -117,4 +114,5 @@ describe('LazyDataFrame', function () {
 			[22, 200],
 		]);
 	});
+	
 });
