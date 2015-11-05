810815805fa070f25a1a19c86c072be80c3b9975
Ashley Davis <ashley.davis@realseriousgames.com>
2015-11-05 14:51:10 +1000

    New example (not yet working) for simple plotting in browser with dates.

---
+++ examples/2. plot - in browser - with dates/app.js
@@ -0,0 +1,47 @@
+'use strict'
+
+$(function() {
+	
+	//
+	// Helper function for plotting.
+	//
+	var plot = function (id, indexColumnName, dataFrame) {
+
+		var remainingColumnNames = dataFrame.dropColumn(indexColumnName).getColumnNames();
+		var flotSeries = E.from(remainingColumnNames)
+			.select(function (columnName) {
+				var seriesData = dataFrame
+					.subset([indexColumnName, columnName])
+					.values();
+				seriesData = E.from(seriesData)
+					.select(function (entry) {
+						return [entry[0].getTime(), entry[1]];
+					})
+					.toArray();
+				return {
+					label: columnName,
+					data: seriesData,
+				};
+			})
+			.toArray();
+
+		$.plot(id, flotSeries);
+	};
+	
+	// 
+	// Create a simple data frame.
+	//
+	var values = E
+		.range(0, 14)
+		.select(function (i) {
+			return [new Date(2015, 3, i), Math.sin(i), Math.cos(i)];
+		})
+		.toArray();
+
+	var dataFrame = new panjas.DataFrame(["Date", "Sin", "Cos"], values);
+	
+	//
+	// Plot the data frame.
+	//
+	plot('#placeholder', "Date", dataFrame);
+});
