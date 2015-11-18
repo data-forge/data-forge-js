208e9353e16f8f8881d0aa03eda4786b0d806759
Ashley Davis <ashley.davis@realseriousgames.com>
2015-11-18 16:49:45 +1000

    Removed old code.

--- src/basecolumn.js
+++ src/basecolumn.js
@@ -222,61 +222,6 @@ BaseColumn.prototype.orderByDescending = function (sortSelector) {
 	return orderBy(self, 'orderByDescending', sortSelector);
 };
 
-
-/*fio:
-//
-// Orders a series based on values in asscending order.
-//
-var order = function (self, sortMethod) {
-
-	assert.isObject(self);
-	assert.isString(sortMethod);
-	assert(sortMethod === 'orderBy' || sortMethod === 'orderByDescending');
-	
-	var cachedSorted = null;
-	
-	//
-	// Lazily execute the sort when needed.
-	//
-	var executeLazySort = function () {
-		if (!cachedSorted) {
-			cachedSorted = E.from(self.getValues())
-				[sortMethod](function (value) {
-					return value;
-				})
-				.toArray();
-		}
-		
-		return cachedSorted;
-	}
-	
-	var LazyColumn = require('./lazycolumn'); // Require here to prevent circular ref.
-
-	return new LazyColumn(
-		self.getName(),
-		function () {
-			return executeLazySort();			
-		}
-	);
-};
-
-
- * Orders a series based on values in asscending order.
-
-BaseColumn.prototype.order = function () {
-	var self = this;
-	return order(self, 'orderBy');
-};
-
-
- * Orders a series based on values in descending order.
-
-BaseColumn.prototype.orderDescending = function () {
-	var self = this;
-	return order(self, 'orderByDescending');
-};
-*/
-
 /**
  * Get a subset of rows from the column.
  *
