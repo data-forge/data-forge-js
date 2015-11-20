80cc8a8ddc253813d48d121ebbe12919b0266fb9
Ashley Davis <ashley.davis@realseriousgames.com>
2015-11-20 10:00:58 +1000

    Added a new example for loading data from mongodb.

---
+++ examples/11. load from mongodb/index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var panjas = require("../../index.js");
+var json = require('../../fmt/json');
+var mongo = require('../../datasource/mongo/mongo');
+var csv = require('../../fmt/csv');
+var file = require('../../datasource/file');
+
+panjas
+	.from(mongo({
+		host: 'localhost:27017',
+		db: 'somed-b',
+		collection: 'some-collection',
+	}))
+	.as(json())
+	.then(function (dataFrame) {
+
+		console.log(dataFrame.toString());
+
+		var dataRange = dataFrame.skip(5).take(10);
+		console.log(dataRange.toString());
+
+		var subset = dataFrame.getColumnsSubset(['SomeColumn', 'SomeOtherColumn']);
+		console.log(subset.toString());
+
+		return subset.as(csv()).to(file('test.csv'));
+	})
+	.catch(function (err) {
+		console.error((err && err.stack) || err);
+	});
+
