307326695bced6a2f05ec25571accb78bc020f31
Ashley Davis <ashley.davis@realseriousgames.com>
2015-11-23 16:51:24 +1000

    Tests for loading from json data.

---
+++ test/fmt.json.test.js
@@ -0,0 +1,82 @@
+'use strict';
+
+describe('fmt/json', function () {
+
+	var panjas = require('../index');
+	var json = require('../fmt/json');
+
+	var expect = require('chai').expect;
+
+	it('can load from empty json array', function () {
+
+		var options = {};
+		var jsonFormatter = json(options);
+
+		var jsonData = [];
+		var dataFrame = jsonFormatter.from(jsonData);
+
+		expect(dataFrame.getColumnNames().length).to.eql(0);
+		expect(dataFrame.getValues().length).to.eql(0);
+	});
+
+	it('can load from array of empty objects', function () {
+
+		var options = {};
+		var jsonFormatter = json(options);
+
+		var jsonData = [{}, {}];
+		var dataFrame = jsonFormatter.from(jsonData);
+
+		expect(dataFrame.getColumnNames().length).to.eql(0);
+		expect(dataFrame.getValues()).to.eql([
+			[],
+			[],
+		]);
+	});
+
+	it('can load from json array', function () {
+
+		var options = {};
+		var jsonFormatter = json(options);
+
+		var jsonData = [
+			{
+				'Column1': 'A',
+				'Column2': 1
+			},
+			{
+				'Column1': 'B',
+				'Column2': 2
+			}
+		];
+		var dataFrame = jsonFormatter.from(jsonData);
+
+		expect(dataFrame.getColumnNames()).to.eql(['Column1', 'Column2']);
+		expect(dataFrame.getValues()).to.eql([
+			['A', 1],
+			['B', 2],
+		]);
+	});
+
+	it('uneven columns results in undefined values', function () {
+
+		var options = {};
+		var jsonFormatter = json(options);
+
+		var jsonData = [
+			{
+				'Column1': 'A',
+			},
+			{
+				'Column2': 2
+			}
+		];
+		var dataFrame = jsonFormatter.from(jsonData);
+
+		expect(dataFrame.getColumnNames()).to.eql(['Column1', 'Column2']);
+		expect(dataFrame.getValues()).to.eql([
+			['A', undefined],
+			[undefined, 2],
+		]);
+	});	
+});
