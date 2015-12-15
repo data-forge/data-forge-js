512d905256c35b07fc450a10b1a75823d0635502
Ashley Davis <ashley.davis@realseriousgames.com>
2015-12-15 16:12:24 +1000

    Small refactor of tests.

--- test/dataframe.test.js
+++ test/dataframe.test.js
@@ -7,51 +7,36 @@ describe('DataFrame', function () {
 	
 	var expect = require('chai').expect;
 	
-	var initExampleDataFrame = function () {
-		return new dataForge.DataFrame(
-			[
-				"Date",
-				"Value1",
-				"Value2",
-				"Value3",
-			],
-			[
-				[new Date(1975, 24, 2), 100, 'foo', 11],
-				[new Date(2015, 24, 2), 200, 'bar', 22],
-			]
-		);		
+	var initExampleDataFrame = function (columns, rows) {
+		return new dataForge.DataFrame(columns, rows);
 	}; 
 
 	it('can get columns', function () {
-		
-		var dataFrame = initExampleDataFrame();
-		
-		expect(dataFrame.getColumnNames()).to.eql([
-			"Date",
-			"Value1",
-			"Value2",
-			"Value3",
-		]);		
+
+		var columns = ["Date", "Value1", "Value2","Value3" ];	
+		var dataFrame = initExampleDataFrame(columns, []);
+		expect(dataFrame.getColumnNames()).to.eql(columns);
 	});
 
-	it('can get values', function () {
+	it('can get rows', function () {
 		
-		var dataFrame = initExampleDataFrame();
-		
-		expect(dataFrame.getValues()).to.eql(			[
+		var columns = ["Date", "Value1", "Value2","Value3" ];	
+		var rows = [
 			[new Date(1975, 24, 2), 100, 'foo', 11],
 			[new Date(2015, 24, 2), 200, 'bar', 22],
-		]);		
+		];
+		var dataFrame = initExampleDataFrame(columns, rows);
+		expect(dataFrame.getValues()).to.eql(rows);
 	});
 
 	it('default index is generated', function () {
 
-		var dataFrame = initExampleDataFrame();
-
-		expect(dataFrame.getIndex().getValues()).to.eql([
-			0,
-			1
-		]);
+		var columns = ["Date", "Value1", "Value2","Value3" ];	
+		var rows = [
+			[new Date(1975, 24, 2), 100, 'foo', 11],
+			[new Date(2015, 24, 2), 200, 'bar', 22],
+		];
+		var dataFrame = initExampleDataFrame(columns, rows);
+		expect(dataFrame.getIndex().getValues()).to.eql([0, 1 ]);
 	});
-	
 });
