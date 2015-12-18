9298f1e9190650a5230ad96e190ce71629296485
Ashley Davis <ashley@codecapers.com.au>
2015-12-18 20:50:04 +1000

    Fixes to code examples.

--- examples/10. merge_prices_and_divs/index.js
+++ examples/10. merge_prices_and_divs/index.js
@@ -1,72 +1,35 @@
 'use strict';
 
 var dataForge = require("../../index.js");
-var csv = require('../../format/csv');
-var file = require('../../source/file');
-
 var glob = require('glob');
 var E = require('linq');
-var assert = require('chai').assert;
 
 //
 // Load as single CSV file containing share prices.
 //
 var loadSharePricesFile = function (filePath) {
 	assert.isString(filePath);
-	
-	return dataForge.from(file(filePath)).as(csv());
+
+	return dataForge.fromCSV(fs.readFileSync(filePath, 'utf8'));
 };
 
 //
 // Load a directory full of files containing share prices.
 // 
 var loadSharePrices = function () {
-	var filePaths = glob.sync("./data/prices/*");
-	var loadFilePromises = E.from(filePaths).select(loadSharePricesFile).toArray();
-	return Promise.all(loadFilePromises)
-		.then(function (dataFrames) {
-			// Concatenate all data frames.
-			return E.from(dataFrames)
-				.aggregrate(function (previous, next) {
-					return previous.concat(next);
-				});
-		});
+	var filePaths = glob.sync("./prices/*");
+	var loaded = E.from(filePaths).select(loadSharePricesFile).toArray();
+	return dataForge.concat(loaded);
 };
 
-//
-// Load dividends from a CSV file.
-//
-var loadDividends = function (filePath) {
-	assert.isString(filePath);
-	
-	return dataForge.from(file, filePath).as(csv);
-};
+var prices = loadSharePrices();
+var dividends = dataForge.fromCSV(fs.readFileSync('dividends.csv', 'utf8'));
 
-//
-// Save data frame to a CSV file.
-//
-var saveFile = function (dataFrame, filePath) {
-	assert.isObject(dataFrame);
-	assert.isString(filePath);
-
-	return dataFrame.as(csv()).to(file(filePath));
-};
-
-loadSharePrices()
-	.then(function (sharePricesDataFrame) {
-
-		return loadDividends('dividends.csv')
-			.then(function (dividendsDataFrame) {
-				return dataForge.merge(sharePricesDataFrame, dividendsDataFrame, {
-						left: "date",
-						right: "ex date",
-						how: 'inner',
-					});
-			});
-	})
-	.then(function (dataFrame) {
-		return saveFile(dataFrame, 'output.csv');
-	})
-	.catch(function (err) {
-		console.error(err.stack);
+var merged = dataForge
+	.merge(sharePricesDataFrame, dividendsDataFrame, {
+		left: "date",
+		right: "ex date",
+		how: 'inner',
 	});
+
+fs.writeFileSync('output.csv', merged.toCSV());
