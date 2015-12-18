acb92ed33c256d84782c95cecc657229e53a3a18
Ashley Davis <ashley@codecapers.com.au>
2015-12-18 20:56:57 +1000

    Simplified code example.

--- examples/6. concatenate_csvs/index.js
+++ examples/6. concatenate_csvs/index.js
@@ -2,17 +2,13 @@
 
 var dataForge = require("../../index.js");
 var fs = require('fs');
-
 var glob = require('glob');
 var E = require('linq');
-var assert = require('chai').assert;
 
 //
 // Load as single CSV file containing share prices.
 //
 var loadSharePricesFile = function (filePath) {
-	assert.isString(filePath);
-
 	return dataForge.fromCSV(fs.readFileSync(filePath, 'utf8'));
 };
 
@@ -25,15 +21,5 @@ var loadSharePrices = function () {
 	return dataForge.concat(loaded);
 };
 
-//
-// Save data frame to a CSV file.
-//
-var saveSharePricesFile = function (dataFrame, filePath) {
-	assert.isObject(dataFrame);
-	assert.isString(filePath);
-
-	fs.writeFileSync(filePath, dataFrame.toCSV());
-};
-
 var dataFrame = loadSharePrices();
-saveSharePricesFile(dataFrame, 'output.csv');
+fs.writeFileSync('output.csv', dataFrame.toCSV());
