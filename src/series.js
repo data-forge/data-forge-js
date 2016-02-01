d77422c3f1ba2cf4fd5a54dec07005277142d7e1
Ashley Davis <ashley@codecapers.com.au>
2016-02-01 14:49:05 +1000

    Can now pass an array iterable into Series constructor.

--- src/series.js
+++ src/series.js
@@ -8,6 +8,9 @@ var BaseSeries = require('./baseseries');
 var LazyIndex = require('./lazyindex');
 var Index = require('./index');
 var ArrayIterator = require('./iterators/array');
+var ArrayIterable = require('./iterables/array');
+var checkIterable = require('./iterables/check');
+var validateIterable = require('./iterables/validate');
 
 var assert = require('chai').assert;
 var E = require('linq');
@@ -21,9 +24,19 @@ var Series = function (config) {
 	var self = this;
 
 	if (config) {
-		assert.isArray(config.values, "Expected 'values' field of 'config' parameter to Series constructor be an array.");
 
-		self._values = config.values;
+		if (!config.values) {
+			throw new Error("Expected 'values' field to be set on 'config' parameter to Series constructor.");
+		}
+
+		if (checkIterable(config.values)) {
+			self._iterable = config.values;
+		}
+		else {
+			assert.isArray(config.values, "Expected 'values' field of 'config' parameter to Series constructor be an array or an iterable.");
+
+			self._iterable = new ArrayIterable(config.values);
+		}		
 
 		if (config.index) {
 			assert.isObject(config.index, "Expected 'index' parameter to Series constructor to be an object.");
@@ -34,15 +47,22 @@ var Series = function (config) {
 			// Generate the index.
 			self._index = new LazyIndex(
 					function () {
-						return new ArrayIterator(E.range(0, config.values.length).toArray());
+						var iterator = self._iterable.getIterator();
+						var length = 0;
+						while (iterator.moveNext()) {
+							++length;
+						}
+						return new ArrayIterator(E.range(0, length).toArray());
 					}
 				);
 		}
 	}
 	else {
-		self._values = [];
+		self._iterable = new ArrayIterable([]);
 		self._index = new Index([]);
 	}
+
+	validateIterable(self._iterable);
 };
 
 var parent = inherit(Series, BaseSeries);
@@ -52,7 +72,7 @@ var parent = inherit(Series, BaseSeries);
  */
 Series.prototype.getIterator = function () {
 	var self = this;
-	return new ArrayIterator(self._values);
+	return self._iterable.getIterator();
 };
 
 /**
--- test/series.test.js
+++ test/series.test.js
@@ -4,6 +4,7 @@
 describe('Series', function () {
 	
 	var dataForge = require('../index');
+	var ArrayIterable = require('../src/iterables/array');
 	
 	var expect = require('chai').expect; 
 	
@@ -28,6 +29,16 @@ describe('Series', function () {
 	it('can get column values', function () {
 		
 		var column = new dataForge.Series({ values: [100, 200] });
+		expect(column.toValues()).to.eql([			
+			100,
+			200			
+		]);		
+	});
+
+	it('can specify values as an iterable', function () {
+		
+		var iterable = new ArrayIterable([100, 200]);
+		var column = new dataForge.Series({ values: iterable });
 		expect(column.toValues()).to.eql([			
 			100,
 			200			
