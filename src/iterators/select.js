a97d7684c2cbce3e296593b43125d4e60f14e993
Ashley Davis <ashley@codecapers.com.au>
2016-02-04 14:23:01 +1000

    Refactor so that iterators only depend on other iterators (rather than depending on iterables). This simplifies things and doesn't affect laziness.

--- src/iterators/select.js
+++ src/iterators/select.js
@@ -4,33 +4,22 @@ var assert = require('chai').assert;
 var E = require('linq');
 
 var validateIterator = require('./validate');
-var validateIterable = require('../iterables/validate');
-
 
 //
 // An iterator that can step multiple other iterators at once.
 //
-var SelectIterator = function (iterable, selector) {
+var SelectIterator = function (iterator, selector) {
 
 	var self = this;
 
-	validateIterable(iterable);
+	validateIterator(iterator);
 	assert.isFunction(selector);
 
-	var iterator = null;
-
 	self.moveNext = function () {				
-		if (!iterator) {
-			iterator = iterable.getIterator();
-		}
-
 		return iterator.moveNext();
 	};
 
 	self.getCurrent = function () {
-		if (!iterator) {
-			return undefined;
-		}
 		return selector(iterator.getCurrent());
 	};
 };
---
+++ test/select-iterator.test.js
@@ -0,0 +1,99 @@
+'use strict';
+
+describe('select iterator', function () {
+
+	var SelectIterator = require('../src/iterators/select');
+	var ArrayIterator = require('../src/iterators/array');
+	var expect = require('chai').expect;
+
+	it('result is undefined before moving to first element', function () {
+
+		var mockIterator = {
+			moveNext: function () {
+				return false;
+			},
+
+			getCurrent: function () {
+				return null;
+			},
+		};
+		var select = new SelectIterator(mockIterator, function () {});
+		expect(select.getCurrent()).to.be.undefined;
+	});
+
+	it('completes straight way when there are no child iterators', function () {
+
+		var mockIterator = {
+			moveNext: function () {
+				return false;
+			},
+
+			getCurrent: function () {
+				return null;
+			},
+		};
+		var select = new SelectIterator(mockIterator, function () {});
+		expect(select.moveNext()).to.eql(false);
+	});
+
+	it('completes when child iterator is complete', function () {
+
+		var mockIterator = {
+			moveNext: function () {
+				return false;
+			},
+
+			getCurrent: function () {
+				return null;
+			},
+		};
+
+		var select = new SelectIterator(mockIterator, function () {});
+		expect(select.moveNext()).to.eql(false);
+	});
+
+	it('continues while child iterator is not complete', function () {
+
+		var mockIterator = {
+			moveNext: function () {
+				return true;
+			},
+
+			getCurrent: function () {
+				return null;
+			},
+		};
+
+		var select = new SelectIterator(mockIterator, function () {});
+		expect(select.moveNext()).to.eql(true);
+		expect(select.moveNext()).to.eql(true);
+		expect(select.moveNext()).to.eql(true);
+	});
+
+	it('output is transformed by selector', function () {
+
+		var original = { something: 1 };
+		var transformed = { somethingElse: 2 };
+
+		var mockIterator = {
+			moveNext: function () {
+				return true;
+			},
+
+			getCurrent: function () {
+				return original;
+			},
+		};
+
+		var select = new SelectIterator(mockIterator, function (input) {
+				expect(input).to.equal(original);
+
+				return transformed;
+			});
+		
+		select.moveNext();
+		expect(select.getCurrent()).to.equal(transformed);
+	});
+
+
+});
