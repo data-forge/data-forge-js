7ba4ca398449b9203e31b962b2b161574eedc675
Ashley Davis <ashley.davis@realseriousgames.com>
2016-02-05 16:58:36 +1000

    Added a take iterator.

---
+++ src/iterators/take.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var assert = require('chai').assert;
+var validateIterator = require('./validate');
+
+//
+// Data-forge enumerator for iterating a standard JavaScript array.
+//
+var TakeIterator = function (iterator, takeAmount) {
+
+	validateIterator(iterator);
+	assert.isNumber(takeAmount);
+
+	var self = this;
+
+	self.moveNext = function () {
+		if (--takeAmount >= 0) {
+			return iterator.moveNext();
+		}
+		return false;
+	};
+
+	self.getCurrent = function () {
+		return iterator.getCurrent();
+	};
+};
+
+module.exports = TakeIterator;
---
+++ test/take-iterator.test.js
@@ -0,0 +1,54 @@
+'use strict';
+
+describe('take iterator', function () {
+
+	var ArrayIterator = require('../src/iterators/array');
+	var TakeIterator = require('../src/iterators/take');
+	var expect = require('chai').expect;
+
+	it('result is undefined before moving to first element', function () {
+
+		var testObject = new TakeIterator(new ArrayIterator([1]), 1);
+		expect(testObject.getCurrent()).to.be.undefined;
+	});
+
+	it('cannot move next for empty enumerator with no take', function () {
+
+		var testObject = new TakeIterator(new ArrayIterator([]), 0);
+		expect(testObject.moveNext()).to.eql(false);
+	});
+
+	it('cannot move next for empty enumerator with take', function () {
+
+		var testObject = new TakeIterator(new ArrayIterator([]), 2);
+		expect(testObject.moveNext()).to.eql(false);
+	});
+
+	it('can move when next take amount is more than the contents of the child iterator', function () {
+
+		var testObject = new TakeIterator(new ArrayIterator([1]), 1);
+		expect(testObject.moveNext()).to.eql(true);
+	});
+
+	it('cannot move next when take is 0', function () {
+
+		var testObject = new TakeIterator(new ArrayIterator([1]), 0);
+		expect(testObject.moveNext()).to.eql(false);
+	});
+
+	it('can get current value.', function () {
+
+		var testObject = new TakeIterator(new ArrayIterator([1, 2, 3, 4, 5]), 3);
+		expect(testObject.moveNext()).to.eql(true);
+		expect(testObject.getCurrent()).to.eql(1);
+
+		expect(testObject.moveNext()).to.eql(true);
+		expect(testObject.getCurrent()).to.eql(2);
+
+		expect(testObject.moveNext()).to.eql(true);
+		expect(testObject.getCurrent()).to.eql(3);
+
+		expect(testObject.moveNext()).to.eql(false);
+	});
+
+});
