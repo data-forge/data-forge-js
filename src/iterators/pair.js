1854e956fed13c73ade5477891162491b3ceadcb
Ashley Davis <ashley.davis@realseriousgames.com>
2016-03-16 16:35:24 +1000

    Added pair iterator to replace multi iterator for better performance when iterating the index/value pairs that underlie DataFrame and Series.

---
+++ src/iterators/pair.js
@@ -0,0 +1,64 @@
+'use strict';
+
+var assert = require('chai').assert;
+var E = require('linq');
+
+var validateIterator = require('./validate');
+
+
+//
+// An iterator that can step multiple other iterators at once.
+//
+var PairIterator = function (it1, it2) {
+	validateIterator(it1);
+	validateIterator(it2);
+
+	var self = this;
+	var started = false;
+	var ok = true;
+
+	self.moveNext = function () {	
+
+		if (!started) {
+			started = true;
+		}
+
+		if (!ok) {
+			return false;
+		}
+
+		if (it1.moveNext() && it2.moveNext()) {
+			return true;
+		}
+
+		ok = false;
+		return false;
+	};
+
+	self.getCurrent = function () {
+		if (!started || !ok) {
+			return undefined;
+		}
+
+		return [
+			it1.getCurrent(),
+			it2.getCurrent(),
+		];
+	};
+
+	//
+	// Bake the iterator into an array.
+	//
+	self.realize = function () {
+
+		var output = [];
+
+		while (self.moveNext()) {
+			output.push(self.getCurrent());
+		}
+
+		return output;
+	};
+};
+
+module.exports = PairIterator;
---
+++ test/iterators/pair.test.js
@@ -0,0 +1,155 @@
+'use strict';
+
+describe('pair iterator', function () {
+
+	var PairIterator = require('../../src/iterators/pair');
+	var ArrayIterator = require('../../src/iterators/array');
+	var expect = require('chai').expect;
+
+	it('result is undefined before moving to first element', function () {
+
+		var mockIterator1 = {
+			moveNext: function () {
+				return true;
+			},
+
+			getCurrent: function () {
+				return null;
+			},
+		};
+
+		var mockIterator2 = {
+			moveNext: function () {
+				return true;
+			},
+
+			getCurrent: function () {
+				return null;
+			},
+		};
+
+		var pair = new PairIterator(mockIterator1, mockIterator2);
+		expect(pair.getCurrent()).to.be.undefined;
+	});
+
+	it('result is undefined after last element', function () {
+
+		var mockIterator1 = {
+			moveNext: function () {
+				return false;
+			},
+
+			getCurrent: function () {
+				return null;
+			},
+		};
+
+		var mockIterator2 = {
+			moveNext: function () {
+				return false;
+			},
+
+			getCurrent: function () {
+				return null;
+			},
+		};
+
+		var pair = new PairIterator(mockIterator1, mockIterator2);
+		expect(pair.getCurrent()).to.be.undefined;
+	});
+
+	it('can move iterators forward when not at end', function () {
+
+		var mockIterator1 = {
+			moveNext: function () {
+				return true;
+			},
+
+			getCurrent: function () {
+				return null;
+			},
+		};
+
+		var mockIterator2 = {
+			moveNext: function () {
+				return true;
+			},
+
+			getCurrent: function () {
+				return null;
+			},
+		};
+
+		var pair = new PairIterator(mockIterator1, mockIterator2);
+		expect(pair.moveNext()).to.eql(true);
+	});
+
+	it('completes when first child iterator completes', function () {
+
+		var mockIterator1 = {
+			moveNext: function () {
+				return false;
+			},
+
+			getCurrent: function () {
+				return null;
+			},
+		};
+
+		var mockIterator2 = {
+			moveNext: function () {
+				return true;
+			},
+
+			getCurrent: function () {
+				return null;
+			},
+		};
+
+		var pair = new PairIterator(mockIterator1, mockIterator2);
+		expect(pair.moveNext()).to.eql(false);
+	});
+
+	it('completes when second child iterator completes', function () {
+		var mockIterator1 = {
+			moveNext: function () {
+				return true;
+			},
+
+			getCurrent: function () {
+				return null;
+			},
+		};
+
+		var mockIterator2 = {
+			moveNext: function () {
+				return false;
+			},
+
+			getCurrent: function () {
+				return null;
+			},
+		};
+
+		var pair = new PairIterator(mockIterator1, mockIterator2);
+		expect(pair.moveNext()).to.eql(false);
+	});
+
+	it('can extract current value', function () {
+
+		var pair = new PairIterator(new ArrayIterator([1, 2]), new ArrayIterator([10, 20]));
+		expect(pair.moveNext()).to.eql(true);
+		expect(pair.getCurrent()).to.eql([1, 10]);
+		expect(pair.moveNext()).to.eql(true);
+		expect(pair.getCurrent()).to.eql([2, 20]);
+		expect(pair.moveNext()).to.eql(false);
+		expect(pair.getCurrent()).to.be.undefined;
+	});
+
+	it('can realise', function () {
+
+		var pair = new PairIterator(new ArrayIterator([1, 2]), new ArrayIterator([10, 20]));
+		expect(pair.realize()).to.eql([[1, 10], [2, 20]]);
+	});
+
+});
