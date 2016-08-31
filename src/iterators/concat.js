486b9011d773f37747343ebd3ce63d0b21a7b025
Ashley Davis <ashley.davis@realseriousgames.com>
2016-08-31 16:41:19 +1000

    Manually fixed the concat iterator after a bad revert.

--- src/iterators/concat.js
+++ src/iterators/concat.js
@@ -1,45 +1,48 @@
 'use strict';
 
-var assert = require('chai').assert;
+var E = require('linq');
 
 //
 // An iterator that can step multiple other iterators at once.
 //
-var ConcatIterator = function (iterables) {
-
-	assert.isArray(iterables);
+var MultiIterator = function (iterators) {
 
 	var self = this;
 
-	var workingIterator;
-	var nextIteratorIndex = 0;
+	var curIterator = -1;
 
-	self.moveNext = function () {
+	self.moveNext = function () {				
+		
+		if (iterators.length === 0) {
+			return false;
+		}
 
+		if (curIterator < 0) {
+			++curIterator;
+		}
+		
 		for (;;) {
-			if (workingIterator) {
-				if (workingIterator.moveNext()) {
-					return true; // Moved next on current iterator.
-				}
-			}				
-
-			if (nextIteratorIndex >= iterables.length) {
-				return false; // Reached the end of all iterables.
+			if (iterators[curIterator].moveNext()) {
+				return true;
 			}
 
-			var nextIterable = iterables[nextIteratorIndex++]; // Grab the next iterable.
-			workingIterator = nextIterable.getIterator(); // Materialize an iterator.
+			if (curIterator >= iterators.length-1) {
+				return false;
+			}
+
+			++curIterator;
 		}
 	};
 
 	self.getCurrent = function () {
-		if (!workingIterator) {
+		if (curIterator >= 0) {
+			return iterators[curIterator].getCurrent();
+		}
+		else {
 			return undefined;
 		}
-
-		return workingIterator.getCurrent();
 	};
 
 };
 
-module.exports = ConcatIterator;
+module.exports = MultiIterator;
--- test/iterators/concat.test.js
+++ test/iterators/concat.test.js
@@ -8,14 +8,14 @@ describe('concat iterator', function () {
 
 	it('result is undefined before moving to first element', function () {
 
-		var testObject = new ConcatIterator([]);
-		expect(testObject.getCurrent()).to.be.undefined;
+		var concat = new ConcatIterator([]);
+		expect(concat.getCurrent()).to.be.undefined;
 	});
 
 	it('completes straight way when there are no child iterators', function () {
 
-		var testObject = new ConcatIterator([]);
-		expect(testObject.moveNext()).to.eql(false);
+		var concat = new ConcatIterator([]);
+		expect(concat.moveNext()).to.eql(false);
 	});
 
 	it('completes immediately if child iterators are empty', function () {
@@ -30,12 +30,6 @@ describe('concat iterator', function () {
 			},
 		};
 
-		var mockIterable1 = {
-			getIterator: function () {
-				return mockIterator1;
-			},			
-		};
-
 		var mockIterator2 = {
 			moveNext: function () {
 				return false;
@@ -46,63 +40,31 @@ describe('concat iterator', function () {
 			},
 		};
 
-		var mockIterable2 = {
-			getIterator: function () {
-				return mockIterator2;
-			},			
-		};
-
-		var testObject = new ConcatIterator([mockIterable1, mockIterable2]);
-		expect(testObject.moveNext()).to.eql(false);
+		var concat = new ConcatIterator([mockIterator1, mockIterator2]);
+		expect(concat.moveNext()).to.eql(false);
 	});
 
 	it('completes when all child iterators complete', function () {
 
-		var iterator1 = new ArrayIterator([1, 2]);
-		var iterable1 = {
-			getIterator: function () {
-				return iterator1;
-			},
-		};
-		var iterator2 = new ArrayIterator([3, 4]);
-		var iterable2 = {
-			getIterator: function () {
-				return iterator2;
-			},
-		};
+		var concat = new ConcatIterator([new ArrayIterator([1, 2]), new ArrayIterator([3, 4])]);
+		expect(concat.moveNext()).to.eql(true);
+		expect(concat.getCurrent()).to.eql(1);
 
-		var testObject = new ConcatIterator([iterable1, iterable2]);
-		expect(testObject.moveNext()).to.eql(true);
-		expect(testObject.getCurrent()).to.eql(1);
+		expect(concat.moveNext()).to.eql(true);
+		expect(concat.getCurrent()).to.eql(2);
 
-		expect(testObject.moveNext()).to.eql(true);
-		expect(testObject.getCurrent()).to.eql(2);
+		expect(concat.moveNext()).to.eql(true);
+		expect(concat.getCurrent()).to.eql(3);
 
-		expect(testObject.moveNext()).to.eql(true);
-		expect(testObject.getCurrent()).to.eql(3);
+		expect(concat.moveNext()).to.eql(true);
+		expect(concat.getCurrent()).to.eql(4);
 
-		expect(testObject.moveNext()).to.eql(true);
-		expect(testObject.getCurrent()).to.eql(4);
-
-		expect(testObject.moveNext()).to.eql(false);
+		expect(concat.moveNext()).to.eql(false);
 	});
 
 	it('can always get last item at the end', function () {
 
-		var iterator1 = new ArrayIterator([1, 2]);
-		var iterable1 = {
-			getIterator: function () {
-				return iterator1;
-			},
-		};
-		var iterator2 = new ArrayIterator([3, 4]);
-		var iterable2 = {
-			getIterator: function () {
-				return iterator2;
-			},
-		};
-
-		var testObject = new ConcatIterator([iterable1, iterable2]);
+		var testObject = new ConcatIterator([new ArrayIterator([1, 2]), new ArrayIterator([3, 4])]);
 
 		testObject.moveNext();
 		testObject.moveNext();
